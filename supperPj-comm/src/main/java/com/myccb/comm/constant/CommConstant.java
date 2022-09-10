0aebaba08fca9b2d47c1ad758a36eacd42f73c84
黄弋峰 <1648114703@qq.com>
2022-09-10 16:40:20 +0800

    user== 黄弋峰 初次提交

---
+++ supperPj-comm/src/main/java/com/myccb/comm/constant/CommConstant.java
@@ -0,0 +1,23 @@
+package com.myccb.comm.constant;
+
+/**
+ * 常量类
+ * @author 黄弋峰
+ */
+public class CommConstant {
+    private CommConstant(){
+
+    }
+
+    /**错误码**/
+    private static final String ERROR_CODE = "FFFFFF";
+
+    /**错误信息**/
+    private static final String ERROR_MSG = "系统错误";
+
+    /**成功**/
+    private static final String SUCCESS = "成功";
+
+    /**成功返回码**/
+    private static final String SUCCESS_CODE = "0000";
+}
