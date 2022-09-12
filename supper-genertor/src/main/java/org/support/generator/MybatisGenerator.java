237fb5ed53f51a5bfb80b3040f03bd17d301fd84
黄弋峰 <1648114703@qq.com>
2022-09-12 11:00:02 +0800

    user== 黄弋峰 新增代码生成器，设置依赖（还未能使用）

---
+++ supper-genertor/src/main/java/org/support/generator/MybatisGenerator.java
@@ -0,0 +1,48 @@
+package org.support.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 代码生成器,启动类
+ * @author HYF
+ */
+public class MybatisGenerator {
+
+    private static File configFile;
+    static {
+        String path = System.getProperty("user.dir").concat("\\src\\main\\resources\\generator\\generatorConfiguration.xml");
+        System.out.println(path);
+        configFile = new File(path);
+    }
+
+    public static void main(String[] args)
+            throws IOException,
+            XMLParserException,
+            InvalidConfigurationException,
+            SQLException,
+            InterruptedException{
+        if (!configFile.exists()){
+            System.out.println("配置文件不存在");
+            return;
+        }
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,callback,warnings);
+        myBatisGenerator.generate(null);
+    }
+}
--- supperPj-service/src/main/java/com/myccb/StartApplication.java
+++ supperPj-service/src/main/java/com/myccb/StartApplication.java
@@ -1,5 +1,7 @@
 package com.myccb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,8 +10,10 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 //@ComponentScan(basePackages = {"com.myccb.appmid","com.myccb"})
 public class StartApplication {
+    private static final Logger logger = LoggerFactory.getLogger(StartApplication.class);
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringApplication.class);
 //        ServiceStar
+        logger.info("启动成功！");
     }
 }
