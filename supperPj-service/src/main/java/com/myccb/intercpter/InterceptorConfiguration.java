543f339d66c7d6ab91ba4df1ea4e6c745c1f734a
黄弋峰 <1648114703@qq.com>
2022-10-12 17:57:34 +0800

    user== 黄弋峰 新增拦截器属性配置类

---
+++ supperPj-service/src/main/java/com/myccb/intercpter/InterceptorConfiguration.java
@@ -0,0 +1,11 @@
+package com.myccb.intercpter;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 拦截器属性配置、
+ * @author 黄弋峰
+ */
+@Configuration
+public class InterceptorConfiguration implements WebMvcConfiguration {
+}
