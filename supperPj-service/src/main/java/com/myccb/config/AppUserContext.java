ff6482b051520c09015ba3f5752e923aaf6c6033
黄弋峰 <1648114703@qq.com>
2022-11-16 18:43:14 +0800

    user== 黄弋峰 新增登录校验

---
+++ supperPj-service/src/main/java/com/myccb/config/AppUserContext.java
@@ -0,0 +1,27 @@
+package com.myccb.config;
+
+import com.myccb.bean.db.UserDb;
+
+/**
+ * 登录通过userInfo可获取到登录的用户信息
+ * @author 黄弋峰 2022/11/16
+ */
+public class AppUserContext {
+    private static final ThreadLocal<UserDb> userInfo = new ThreadLocal<>();
+
+    private AppUserContext(){
+
+    }
+
+    public static UserDb getUserDb(){
+        return userInfo.get();
+    }
+
+    public static void setUserDb(UserDb userDb){
+        userInfo.set(userDb);
+    }
+
+    public static void remove(){
+        userInfo.remove();
+    }
+}
